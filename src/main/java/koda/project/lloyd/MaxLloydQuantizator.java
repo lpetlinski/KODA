10359c3113d9a34dd358e1e66bd2d9ae92d2dd79
emiedzinski <edward.miedzinski@gmail.com>
2014-01-28 16:40:05 +0100

    Merge

--- src/main/java/koda/project/lloyd/MaxLloydQuantizator.java
+++ src/main/java/koda/project/lloyd/MaxLloydQuantizator.java
@@ -1,109 +1,113 @@
 package koda.project.lloyd;
 
 /**
- * @author Ĺ�ukasz
+ * @author Łukasz
  * 
  *         Class implementing max lloyd algorithm.
  */
 public class MaxLloydQuantizator {
 
-	public int intervals;
-	public double intervalLevels[];
-	public double thresholds[];
-	public double meanSquareErrors[];
-	public double errorThreshold;
-	public DiscreteProbability probability;
+    public int intervals;
+    public double intervalLevels[];
+    public double thresholds[];
+    public double meanSquareErrors[];
+    public double errorThreshold;
+    public DiscreteProbability probability;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param probability
-	 *            Class implementing discrete probability interface of function
-	 *            to quantize.
-	 * @param intervals
-	 *            Number of intervals to quantize function into.
-	 * @param errorThreshold
-	 *            Minimal error threshold to stop quantization at.
-	 */
-	public MaxLloydQuantizator(DiscreteProbability probability, int intervals,
-			double errorThreshold) {
-		this.probability = probability;
-		this.intervals = intervals;
-		this.errorThreshold = errorThreshold;
-		this.intervalLevels = new double[intervals];
-		this.thresholds = new double[intervals + 1];
-		this.meanSquareErrors = new double[intervals];
-	}
+    /**
+     * Constructor.
+     * 
+     * @param probability
+     *            Class implementing discrete probability interface of function
+     *            to quantize.
+     * @param intervals
+     *            Number of intervals to quantize function into.
+     * @param errorThreshold
+     *            Minimal error threshold to stop quantization at.
+     */
+    public MaxLloydQuantizator(DiscreteProbability probability, int intervals,
+            double errorThreshold) {
+        this.probability = probability;
+        this.intervals = intervals;
+        this.errorThreshold = errorThreshold;
+        this.intervalLevels = new double[intervals];
+        this.thresholds = new double[intervals + 1];
+        this.meanSquareErrors = new double[intervals];
+    }
 
-	/**
-	 * Runs max lloyd quantization algorithm.
-	 * 
-	 * @return Thresholds of optimal quantization.
-	 */
-		double intervalDiff = probability.GetProbabilityLevels()/(double)intervals;
-			intervalLevels[i] = intervalDiff*i + intervalDiff/2;		}
+    /**
+     * Runs max lloyd quantization algorithm.
+     * 
+     * @return Thresholds of optimal quantization.
+     */
+    public double[] RunQuantization() {
 
-		this.thresholds[0] = 0;
-		this.thresholds[intervals] = this.probability.GetProbabilityLevels() - 1;
+        double intervalDiff = probability.GetProbabilityLevels()
+                / (double) intervals;
 
-		double lastMeanSquareError;
-		double actualMeanSquareError = Double.MAX_VALUE;
-		do {
-			lastMeanSquareError = actualMeanSquareError;
-			this.ComputeThresholds();
+        for (int i = 0; i < intervals; i++) {
+            intervalLevels[i] = intervalDiff * i + intervalDiff / 2;
+        }
 
-			this.ComputeIntervals();
+        this.thresholds[0] = 0;
+        this.thresholds[intervals] = this.probability.GetProbabilityLevels() - 1;
 
-			actualMeanSquareError = this.ComputeMeanSquareErrors();
+        double lastMeanSquareError;
+        double actualMeanSquareError = Double.MAX_VALUE;
+        do {
+            lastMeanSquareError = actualMeanSquareError;
+            this.ComputeThresholds();
 
-		} while ((lastMeanSquareError - actualMeanSquareError)
-				/ actualMeanSquareError > errorThreshold);
+            this.ComputeIntervals();
 
-		return this.thresholds;
-	}
+            actualMeanSquareError = this.ComputeMeanSquareErrors();
 
-	/**
-	 * Computes mean-square errors for each interval level.
-	 * 
-	 * @return Sum of all mean square errors.
-	 */
-	private double ComputeMeanSquareErrors() {
-		double result = 0;
-		for (int i = 0; i < this.intervals; i++) {
-			double tmp = 0;
-			for (int j = (int) this.thresholds[i]; j < (int) this.thresholds[i + 1]; j++) {
-				tmp += Math.pow((double) j - this.intervalLevels[i], 2)
-						* this.probability.GetProbability(j);
-			}
-			this.meanSquareErrors[i] = tmp;
-			result += tmp;
-		}
-		return result;
-	}
+        } while ((lastMeanSquareError - actualMeanSquareError)
+                / actualMeanSquareError > errorThreshold);
 
-	/**
-	 * Computes interval levels.
-	 */
-	private void ComputeIntervals() {
-		double lastLevel = 0.0;
-		for (int i = 0; i < this.intervals; i++) {
-			double upper = 0;
-			double lower = 0;
-			for (int j = (int) this.thresholds[i]; j < (int) this.thresholds[i + 1]; j++) {
-				upper += j * this.probability.GetProbability(j);
-				lower += this.probability.GetProbability(j);
-			}
-			this.intervalLevels[i] = (lower == 0.0) ? lastLevel : upper / lower;
-			lastLevel = this.intervalLevels[i];
-		}
-	}
+        return this.thresholds;
+    }
 
-	/**
-	 * Computes thresholds.
-	 */
-	private void ComputeThresholds() {
-		for (int i = 1; i < this.intervals; i++) {
-			this.thresholds[i] = (this.intervalLevels[i] + this.intervalLevels[i - 1]) / 2;
-		}
-	}
+    /**
+     * Computes mean-square errors for each interval level.
+     * 
+     * @return Sum of all mean square errors.
+     */
+    private double ComputeMeanSquareErrors() {
+        double result = 0;
+        for (int i = 0; i < this.intervals; i++) {
+            double tmp = 0;
+            for (int j = (int) this.thresholds[i]; j < (int) this.thresholds[i + 1]; j++) {
+                tmp += Math.pow((double) j - this.intervalLevels[i], 2)
+                        * this.probability.GetProbability(j);
+            }
+            this.meanSquareErrors[i] = tmp;
+            result += tmp;
+        }
+        return result;
+    }
+
+    /**
+     * Computes interval levels.
+     */
+    private void ComputeIntervals() {
+        for (int i = 0; i < this.intervals; i++) {
+            double upper = 0;
+            double lower = 0;
+            for (int j = (int) this.thresholds[i]; j < (int) this.thresholds[i + 1]; j++) {
+                upper += j * this.probability.GetProbability(j);
+                lower += this.probability.GetProbability(j);
+            }
+            this.intervalLevels[i] = upper / lower;
+        }
+    }
+
+    /**
+     * Computes thresholds.
+     */
+    private void ComputeThresholds() {
+        for (int i = 1; i < this.intervals; i++) {
+            this.thresholds[i] = (this.intervalLevels[i] + this.intervalLevels[i - 1]) / 2;
+        }
+    }
 }
